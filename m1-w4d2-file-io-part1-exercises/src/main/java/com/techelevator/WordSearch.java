f2b6ac06175c25e6128a102307135254405a97dc
Lauren Agne <AgneLauren1@gmail.com>
2017-09-20 15:09:27 -0400

    Tuesdays Work

--- m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/WordSearch.java
+++ m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/WordSearch.java
@@ -16,11 +16,13 @@ public class WordSearch {
 		System.out.print("What word would you like to search?:");
 		String wordToSearch = input.nextLine();   
 		
-		//System.out.print("Would you like this search to be case sensitive?: Yes or No");
+		System.out.print("Would you like this search to be case sensitive?: Yes or No");
+		String caseSensitive = input.nextLine(); 
 		//Boolean caseSensitive = input.hasNextLine();   
 		
-		try(Scanner input1 = new Scanner(userFile)){  
+		try(Scanner input1 = new Scanner(userFile)){  // this is already case sensitive
 			int lineCounter = 0;
+			if (caseSensitive.equals("Yes")){
 				while(input1.hasNextLine()){           
 				String line = input1.nextLine(); 
 				lineCounter++;
@@ -28,14 +30,24 @@ public class WordSearch {
 					System.out.print(lineCounter + ") ");
 					System.out.println(line);
 				}
+			}	
+			}else if (caseSensitive.equals("No")){
+				while(input1.hasNextLine()){           
+					String line = input1.nextLine(); 
+					lineCounter++;
+					if (line.toUpperCase().contains(wordToSearch.toUpperCase())){ 
+						System.out.print(lineCounter + ") ");
+						System.out.println(line);
 			}
-			
-		} catch (FileNotFoundException e) {
+			}	
+			}		
+							
+		}catch (FileNotFoundException e) {
 			System.out.println("Your file doesn't exist.");
 		}
-		
+	}
 	}
 
-}
+
 
 
