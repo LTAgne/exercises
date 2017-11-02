4dbd02fb13b974b4ba3ae05636c097ef276703b1
Lauren Agne <AgneLauren1@gmail.com>
2017-11-02 09:22:48 -0400

    Week 10 work

--- m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/dom-exercises.js
+++ m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/dom-exercises.js
@@ -1,45 +1,68 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
 //Locate all <div> elements and add the 'president' class.
 function presentialDivs() {
-
-}
+	//	$('#presidents > div').addClass('president');
+	//  }
+		
+	//	var presidentDivs = document.querySelectorAll('div#presidents > div');
+	//	presidentDivs.forEach(function(element) {
+	//		element.classList.add('president');
+	//	});
+	//}
+	
+	document.querySelectorAll('div#presidents > div').forEach(function(element) {
+		element.classList.add('president');
+	});
+	}
 
 //Locate George Washington and add the 'founding-father' class
-function georgeWashington() {
 
-}
+	function georgeWashington() {
+		$('div#presidents > div:first').addClass('founding-father');
+		}
+	
+	//function georgeWashington() {
+	//document.querySelector('div#presidents > div').classList.add('founding-father');
+	//}
 
 //Locate all Republican presidents and add the 'red' class.
-function republicanPresidents() {
 
-}
+	function republicanPresidents() {
+		$('div#presidents >div.Republican').addClass('red');
+	}
 
+	
 //Locate all Democrat presidents and add the 'blue' class.
-function democratPresidents() {
 
-}
+	function democratPresidents() {
+	$('div#presidents >div.Democratic').addClass('blue');
+	}
 
 //Locate all Federalist presidents and add the 'gold' class.
-function otherPresidents() {
 
-}
+	function otherPresidents() {
+	$('div#presidents >div').not('.Republican').not('.Democratic').addClass('gold');
+	}
 
 //Locate all presidents named 'James' and add the 'james' class.
-function presidentsNamedJames() {
-
-}
+	
+	function presidentsNamedJames() {
+		$("div#presidents h3:contains('James')").addClass('james');
+	}
 
 //Locate each president serving at the beginning of each century and add the 'beginningOfCentury' class.
-function turnOfTheCenturyPresidents() {
-
-}
+	
+	function turnOfTheCenturyPresidents() {
+	 $('h2').next('div').addClass('beginningOfCentury');
+	}
 
 //Locate each president serving at the end of the century and add the 'endOfCentury' class.
-function endOfTheCenturyPresidents() {
-    
-}
-
+	
+	function endOfTheCenturyPresidents() {
+	$('div').prev('h2').addClass('endOfCentury');
+	}
+	
 //Locate all presidents serving in the 1900s who appear on currency and add the 'appearOnCurrency' class.
-function currencyPresidents() {
-
-}
+	function currencyPresidents() {
+	$('h2:contains("1900s")').nextAll('.currency').addClass('appearOnCurrency');
+	}
--- m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
+++ m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
@@ -2,4 +2,27 @@
 
 $(document).ready(function () {
 
+$("#SameShipping").on('change',function(){
+
+if(this.checked == true){
+    $("#ShippingAddress1").val($("#BillingAddress1").val());
+    $("#ShippingAddress2").val($("#BillingAddress2").val());
+    $("#ShippingCity").val($("#BillingCity").val());
+    $("#ShippingState").val($("#BillingState").val());
+    $("#ShippingCity").val($("#BillingCity").val());
+    $("#ShippingPostalCode").val($("#BillingPostalCode").val()); 
+    }
+
+});
+
+$("input[name='ShippingType']").on('click',function(event){
+    var checkBox = $(this); 
+    var checkShipping = checkBox.attr('data-cost');
+$('#shipping > .price').text('$'+ checkShipping); 
+
+
+var subttl = $('#subtotal > .price').text().substring(1);
+$('#grandtotal > .price').text('$' + (parseFloat(checkShipping) + parseFloat(subttl)).toFixed(2));
+});
+
 });
